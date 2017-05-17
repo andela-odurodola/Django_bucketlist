c2be822a99821449b1498480c7df7a0db2415ac5
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-05-17 14:57:51 +0100

    Create a superuser

--- BucketListAPI/bucketlistapp/admin.py
+++ BucketListAPI/bucketlistapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BucketList, BucketListItem, User
 
-# Register your models here.
+admin.site.register(BucketList)
+admin.site.register(BucketListItem)
+admin.site.register(User)
