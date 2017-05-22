e54a461edcfce756d145ca5af41b718098ea8bb2
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-05-22 15:54:53 +0100

    Refactor the serializers class to cater for relations

--- BucketListAPI/bucketlistapp/serializers.py
+++ BucketListAPI/bucketlistapp/serializers.py
@@ -1,21 +1,27 @@
 from rest_framework import serializers
 
-from models import BucketList, BucketListItem, User
-
-
-class BucketListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = BucketList
+from bucketlistapp.models import BucketList, BucketListItem, User
 
 
 class BucketListItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BucketListItem
+        fields = ('name', 'bucketlist_id', 'date_created', 'date_modified', 'done')
+
+
+class BucketListSerializer(serializers.ModelSerializer):
+    items = BucketListItemSerializer(many=True)
+
+
+    class Meta:
+        model = BucketList
+        fields = ('name', 'items', 'date_created', 'date_modified', 'created_by')
+
 
 
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
+        fields = '__all__'
